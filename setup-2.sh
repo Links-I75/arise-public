9dd92c1cc90716cf038ce080918763ac0cb3454b
Mario Spinelli <mario.spinelli@sardamatic.it>
2025-08-07 19:00:30 +0200

    setup 2

--- setup-2.sh
+++ setup-2.sh
@@ -8,8 +8,16 @@ echo Install MicroK8s
 sudo snap install microk8s --classic
 
 echo Wait for MicroK8s to be ready
-microk8s status --wait-ready
+sudo microk8s status --wait-ready
 
+echo === Enable necessary add-ons ===
+sudo microk8s enable dashboard dns storage
+
+# Optional: enable Prometheus for monitoring
+sudo microk8s enable prometheus
+
+# Verify enabled add-ons
+sudo microk8s status
 
 echo Add current user to microk8s group
 sudo usermod -a -G microk8s $USER
@@ -17,13 +25,3 @@ sudo usermod -a -G microk8s $USER
 # Set correct permissions
 sudo chown -f -R $USER ~/.kube
 
-
-
-echo === Enable necessary add-ons ===
-microk8s enable dashboard dns storage
-
-# Optional: enable Prometheus for monitoring
-microk8s enable prometheus
-
-# Verify enabled add-ons
-microk8s status
