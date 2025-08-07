289b988d54c727231a85028c5048a89bef9e02f9
Mario Spinelli <mario.spinelli@sardamatic.it>
2025-08-07 18:36:23 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,15 @@ sudo apt update && sudo apt upgrade -y
 
 # Install essential packages
 sudo apt install -y curl wget git nano vim
+
+
+# Docker
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo usermod -aG docker $USER
